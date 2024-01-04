f1af6bf8290a81da05db83f4164e316752797c7e
Mark <121136565+Gjernslav@users.noreply.github.com>
2024-01-04 12:01:49 -0600

    created function for questions and answers

---
+++ script.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+            {text: "Kalahari", correct: false},
+
+        ] 
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            {text: "Australia", correct: true},
+            {text: "North America", correct: false},
+            {text: "Asia", correct: false},
+            {text: "Africa", correct: false},
+
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            {text: "Shri Lanka", correct: false},
+            {text: "Monaco", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Vatican City", correct: true},
+
+        ]
+    }
+];
